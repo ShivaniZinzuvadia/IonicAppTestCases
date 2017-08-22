ad5186f4128224550fd3e3072c0c57b29ee33aad
DESKTOP-SNCENVF\shivani <shivani@timeloop.in>
2017-08-22 12:28:05 +0530

    Shivani : Call java method through javascript by creating custom plugin.

--- www/js/app.js
+++ www/js/app.js
@@ -5,20 +5,37 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+
+            //Call coolMethod of custom plugin
+            //Method defined in js file in plugin
+            //Js method call coolMethod defined in java
+            window.cordova.plugins.customPlugin.coolMethod("This is the Message that passed to java code through javascript", function (res) {
+                window.plugins.toast.showShortTop(res, function (a) {
+                    /*alert('toast success: ' + JSON.stringify(a));*/
+                }, function (b) {
+                    alert('toast error: ' + JSON.stringify(b));
+                });
+            }, function (err) {
+                window.plugins.toast.showShortTop(err, function (a) {
+                   /* alert('toast success: ' + JSON.stringify(a));*/
+                }, function (b) {
+                    alert('toast error: ' + JSON.stringify(b));
+                });
+            });
+        });
+    })
