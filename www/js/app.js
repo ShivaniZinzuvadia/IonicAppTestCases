8dcea8af85b14b914aff7a84ae0603d306aad1f2
DESKTOP-SNCENVF\shivani <shivani@timeloop.in>
2017-08-22 13:54:06 +0530

    Shivani : Create new file in external storage and read its content

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,62 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+    .controller('HomeCtrl', function ($scope, $cordovaFile, $cordovaFileTransfer) {
+        $scope.downloadImage = function () {
+            var url = encodeURI("http://ec2-35-154-106-170.ap-south-1.compute.amazonaws.com/plant-identification/public/img/filterValue/fv_icon_352.jpg"),
+                filename = "fv_icon_352.jpg",
+                targetPath = encodeURI(cordova.file.externalRootDirectory + 'Android/data/io.ionic.starter/files/' +filename),
+                options = {},
+                trustHosts = true;
+            var permissions = cordova.plugins.permissions;
+            permissions.hasPermission(permissions.READ_EXTERNAL_STORAGE, checkPermissionCallback, null);
+
+            function checkPermissionCallback(status) {
+                if (!status.hasPermission) {
+                    var errorCallback = function () {
+                        console.warn('Storage permission is not turned on');
+                    }
+                    permissions.requestPermission(
+                        permissions.READ_EXTERNAL_STORAGE,
+                        function (status) {
+                            if (!status.hasPermission) {
+                                errorCallback();
+                            } else {
+
+                                var fileDir = cordova.file.externalRootDirectory + 'Android/data/io.ionic.starter/files/';
+                                var fileName = "hello.txt";
+                                var fileData = "Hello written in text file";
+
+                                //Create New text file
+                                $cordovaFile.writeFile(fileDir, fileName, fileData, true).then(function (success) {
+
+                                    //Read content of newly create file
+                                    $cordovaFile.readAsText(fileDir, fileName)
+                                        .then(function (success) {
+                                            window.plugins.toast.showShortTop("Newly created file content:  "+success, function (a) {
+                                            }, function (b) {
+                                                alert('toast error: ' + JSON.stringify(b));
+                                            });
+                                        }, function (error) {
+                                            // error
+                                            window.plugins.toast.showShortTop("Error in reading file " + JSON.stringify(error), function (a) {
+                                            }, function (b) {
+                                                alert('toast error: ' + JSON.stringify(b));
+                                            });
+                                        });
+
+                                }, function (error) {
+                                    window.plugins.toast.showShortTop("Error in writing file " + JSON.stringify(error), function (a) {
+                                    }, function (b) {
+                                        alert('toast error: ' + JSON.stringify(b));
+                                    });
+                                });
+                            }
+                        },
+                        errorCallback);
+                }
+            }
+
+        };
+    });
+
